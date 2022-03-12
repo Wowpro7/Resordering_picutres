792e8a31c0e37079d0575887946b1fcb9f2851b8
Wowpro7 <36337995+Wowpro7@users.noreply.github.com>
2022-03-12 06:08:18 +0300

    Add files via upload
    change the output files type, now can reorder any type of file without change its original type.
    change the use of the list

--- main.py
+++ main.py
@@ -15,11 +15,12 @@ def get_file_metadata(imgs_path, filesnames):
 	for i in filesnames: #create a list of dictionary 
 		file_metadata = dict()
 		item = ns.ParseName(str(i))
-		attr_value = ns.GetDetailsOf(item, 4)
+		attr_value = ns.GetDetailsOf(item, 4)# its 4 - because i know its the right location
 		if attr_value:
 			file_metadata[i] = attr_value
 		#	print(file_metadata)
 		file_metadata_list.append(file_metadata)
+	
 	return file_metadata_list
 
 def sort_by_date(Meta_list):
@@ -42,11 +43,9 @@ def main():
 	dict_images = list()
 	folder_name = input("Write pictures folder name with main.py location:")
 	program_location = os.path.dirname(os.path.abspath(__file__))
-	pictures_path = program_location+ f'\\{folder_name}'
+	pictures_path = f"{program_location}\\{folder_name}"
 	try:
 		files_list = os.listdir(pictures_path)
-		#filter files that dont match, anything but *.jpeg or *.jpg
-		#filtered_list = [i for i in files_list if  (fnmatch.fnmatch(i,'*.jpg') or fnmatch.fnmatch(i,'*.jpeg'))]
 		dict_images = get_file_metadata(pictures_path, files_list)
 		sorted_list = sort_by_date(dict_images)
 		if isinstance(sorted_list,str):# checking variable type matches 'str'
@@ -55,7 +54,8 @@ def main():
 		try:
 			new_name = input("header word/ letter for pictures: ")
 			for i in range(len(sorted_list)):
-				os.rename(pictures_path + '\\'+ str(list(sorted_list[i].keys()))[2:-2],pictures_path +  f"\\{new_name}{i+1}.jpg")
+			
+				os.rename(f"{pictures_path}\\{list(sorted_list[i].keys())[0]}",  f"{pictures_path}\\{new_name}{i+1}.{list(sorted_list[i].keys())[0].split('.')[-1]}")
 		except FileExistsError:
 			print("Couldn't overed existing file, try a different name")
 	except FileNotFoundError:
